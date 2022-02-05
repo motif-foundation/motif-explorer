deceaf7611d408eef3bf9ba6f8e43bccab83cc90
Steve <johnw@motif.foundation>
2022-02-05 16:07:34 +0300

    Initial commit

---
+++ config/dev.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+# DO NOT make it `:debug` or all Ecto logs will be shown for indexer
+config :logger, :console, level: :info
+
+config :logger, :ecto,
+  level: :debug,
+  path: Path.absname("logs/dev/ecto.log")
+
+config :logger, :error, path: Path.absname("logs/dev/error.log")
+ config :block_scout_web, BlockScoutWeb.Endpoint,
+    http: [port: 4020],
+    https: [
+      port: 4021,
+      cipher_suite: :strong,
+      certfile: "priv/cert/selfsigned.pem",
+      keyfile: "priv/cert/selfsigned_key.pem"
+    ]
